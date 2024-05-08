deaa14fe5740580a47b754bbc5669c1a38baae18
denisbelenko <denisdel123@>
2024-05-08 17:16:45 +0700

    Реализовал кастомную команду для создания супер пользователя

---
+++ usersApp/management/commands/csu.py
@@ -0,0 +1,21 @@
+import os
+
+from django.core.management import BaseCommand
+from dotenv import load_dotenv
+from usersApp.models import User
+load_dotenv()
+EMAIL_SUPERUSER = os.environ.get('EMAIL_SUPERUSER')
+PASS_SUPERUSER = os.environ.get('PASS_SUPERUSER')
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        superuser = User.objects.create(
+            first_name='Admin',
+            email=EMAIL_SUPERUSER,
+            is_active=True,
+            is_staff=True,
+            is_superuser=True,
+        )
+        superuser.set_password(PASS_SUPERUSER)
+        superuser.save()
